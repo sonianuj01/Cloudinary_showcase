78183cd85047b126f145dc10137e0b67f0226069
sonianuj01 <anujsonist01@gmail.com>
2026-06-27 11:08:53 +0530

    fix: correct import and usage of relativeTime in VideoCard component

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -2,11 +2,11 @@ import React, {useState, useEffect, useCallback} from 'react'
 import {getCldImageUrl, getCldVideoUrl} from "next-cloudinary"
 import { Download, Clock, FileDown, FileUp } from "lucide-react";
 import dayjs from 'dayjs';
-import realtiveTime from "dayjs/plugin/relativeTime"
+import relativeTime from "dayjs/plugin/relativeTime"
 import {filesize} from "filesize"
 import { Video } from "@prisma/client";
 
-dayjs.extend(realtiveTime)
+dayjs.extend(relativeTime)
 
 interface VideoCardProps {
     video: Video;
